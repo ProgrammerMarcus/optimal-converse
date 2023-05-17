14918d976d1bf468ba1909f7a32c62d1d1fcab40
Marcus Haapasaari Lindgren <maha2016@studetn.miun.se>
2023-05-17 17:43:47 +0200

    Merged, or something.

--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline
 from imblearn.under_sampling import RandomUnderSampler
@@ -42,92 +43,19 @@ df = df.replace(creative_conflict, 'Creative Conflict')
 y = df['Name']
 X = df['Coded Text']
 
-# folds
-
 skf = StratifiedKFold(n_splits=5, random_state=66, shuffle=True)
 
-steps = [
-    ('vect', TfidfVectorizer(max_features=1000, min_df=0, max_df=0.9)),
-    ('model', RandomForestClassifier()),
-]
-pipeline = Pipeline(steps=steps)
+def fold_performance():
+    """
+    Prints the statistics of each fold in Stratified 5-fold cross-validation in LaTex format.
 
-# evaluate pipeline
-scores = cross_validate(pipeline, X, y, scoring=['accuracy', 'precision_macro', 'recall_macro'], cv=skf, n_jobs=-1)
+    The function retrieves performance scores such as accuracy, precision, and recall from the dictionary 'scores'.
+    It creates a pandas DataFrame using the scores and then prints the DataFrame in LaTeX format.
 
-table = {'Accuracy': scores["test_accuracy"],
-         'Precision': scores["test_precision_macro"],
-         'Recall': scores["test_recall_macro"]}
+    """
 
-data_folds = pd.DataFrame(data=table)
-
-print(data_folds.to_latex(caption="Fold Performance",
-                          index=False,
-                          formatters={"name": str.upper},
-                          float_format="{:.4f}".format))
-
-# classifiers
-
-classifiers = [
-    KNeighborsClassifier(),
-    SVC(kernel="linear"),
-    SVC(kernel="rbf", gamma="scale"),
-    SVC(kernel="rbf", gamma=1),
-    MultinomialNB(),
-    DecisionTreeClassifier(),
-    RandomForestClassifier(),
-    MLPClassifier(alpha=1, max_iter=1000),
-    AdaBoostClassifier(),
-]
-
-names = ["5-Neighbour", "Linear SVM", "RBF SVM (scale)", "RBF SVM (1)", "Multinomial Naïve Bayes", "Decision Tree", "Random Forest", "MLP", "AdaBoost"]
-accuracy = []
-precision = []
-recall = []
-
-for c, n in zip(classifiers, names):
     steps = [
         ('vect', TfidfVectorizer(max_features=1000, min_df=0, max_df=0.9)),
-        ('model', c),
-    ]
-    pipeline = Pipeline(steps=steps)
-
-    # evaluate pipeline
-    scores = cross_validate(pipeline, X, y, scoring=['accuracy', 'precision_macro', 'recall_macro'], cv=skf, n_jobs=-1)
-
-    accuracy += [(sum(scores["test_accuracy"]) / len(scores["test_accuracy"]))]
-    precision += [sum(scores["test_precision_macro"]) / len(scores["test_precision_macro"])]
-    recall += [(sum(scores["test_recall_macro"]) / len(scores["test_recall_macro"]))]
-
-table = {'Name': names,
-         'Accuracy': accuracy,
-         'Precision': precision,
-         'Recall': recall}
-
-data_folds = pd.DataFrame(data=table)
-
-print(data_folds.to_latex(caption="Classifier Performance",
-                          index=False,
-                          formatters={"name": str.upper},
-                          float_format="{:.4f}".format))
-
-# vectorizers
-
-vecotorizers = [
-    CountVectorizer(max_features=1000, min_df=0, max_df=0.9),
-    CountVectorizer(max_features=1000, min_df=0, max_df=0.9, stop_words=stopwords.words('english')),
-    TfidfVectorizer(max_features=1000, min_df=0, max_df=0.9),
-    TfidfVectorizer(max_features=1000, min_df=0, max_df=0.9, stop_words=stopwords.words('english')),
-]
-
-names = ["Bag-of-words", "Bag-of-words (Remove Stopwords)", "TF-IDF", "TF-IDF (Remove Stopwords)"]
-accuracy = []
-precision = []
-recall = []
-
-for v, n in zip(vecotorizers, names):
-    steps = [
-        ('vect', v),
         ('model', RandomForestClassifier()),
     ]
     pipeline = Pipeline(steps=steps)
@@ -135,57 +63,165 @@ for v, n in zip(vecotorizers, names):
     # evaluate pipeline
     scores = cross_validate(pipeline, X, y, scoring=['accuracy', 'precision_macro', 'recall_macro'], cv=skf, n_jobs=-1)
 
-    accuracy += [(sum(scores["test_accuracy"]) / len(scores["test_accuracy"]))]
-    precision += [sum(scores["test_precision_macro"]) / len(scores["test_precision_macro"])]
-    recall += [(sum(scores["test_recall_macro"]) / len(scores["test_recall_macro"]))]
+    table = {'Accuracy': scores["test_accuracy"],
+             'Precision': scores["test_precision_macro"],
+             'Recall': scores["test_recall_macro"]}
 
-table = {'Name': names,
-         'Accuracy': accuracy,
-         'Precision': precision,
-         'Recall': recall}
+    data_folds = pd.DataFrame(data=table)
 
-data_folds = pd.DataFrame(data=table)
+    print(data_folds.to_latex(caption="Fold Performance",
+                              index=False,
+                              formatters={"name": str.upper},
+                              float_format="{:.4f}".format))
 
-print(data_folds.to_latex(caption="Vectorizer Performance",
-                          index=False,
-                          formatters={"name": str.upper},
-                          float_format="{:.4f}".format))
 
-# samplers
+def classifier_performance():
+    """
+    Prints the statistics of each classifier in the list 'classifiers' in LaTex format.
 
-samplers = [
-    SMOTE(),
-    RandomUnderSampler()
-]
-
-names = ["SMOTE oversampling", "Random Undersampling"]
-accuracy = []
-precision = []
-recall = []
-
-for s, n in zip(samplers, names):
-    steps = [
-        ('vect', TfidfVectorizer(max_features=1000, min_df=0, max_df=0.9)),
-        ('samper', s),
-        ('model', RandomForestClassifier()),
+    The function retrieves performance scores such as accuracy, precision, and recall from the dictionary 'scores'.
+    It creates a pandas DataFrame using the scores and then prints the DataFrame in LaTeX format.
+    """
+    classifiers = [
+        KNeighborsClassifier(),
+        SVC(kernel="linear"),
+        SVC(kernel="rbf", gamma="scale"),
+        SVC(kernel="rbf", gamma=1),
+        MultinomialNB(),
+        DecisionTreeClassifier(),
+        RandomForestClassifier(),
+        MLPClassifier(alpha=1, max_iter=1000),
+        AdaBoostClassifier(),
     ]
-    pipeline = Pipeline(steps=steps)
 
-    # evaluate pipeline
-    scores = cross_validate(pipeline, X, y, scoring=['accuracy', 'precision_macro', 'recall_macro'], cv=skf, n_jobs=-1)
+    names = ["5-Neighbour", "Linear SVM", "RBF SVM (scale)", "RBF SVM (1)", "Multinomial Naïve Bayes", "Decision Tree",
+             "Random Forest", "MLP", "AdaBoost"]
+    accuracy = []
+    precision = []
+    recall = []
 
-    accuracy += [(sum(scores["test_accuracy"]) / len(scores["test_accuracy"]))]
-    precision += [sum(scores["test_precision_macro"]) / len(scores["test_precision_macro"])]
-    recall += [(sum(scores["test_recall_macro"]) / len(scores["test_recall_macro"]))]
+    for c, n in zip(classifiers, names):
+        steps = [
+            ('vect', TfidfVectorizer(max_features=1000, min_df=0, max_df=0.9)),
+            ('model', c),
+        ]
+        pipeline = Pipeline(steps=steps)
 
-table = {'Name': names,
-         'Accuracy': accuracy,
-         'Precision': precision,
-         'Recall': recall}
+        # evaluate pipeline
+        scores = cross_validate(pipeline, X, y, scoring=['accuracy', 'precision_macro', 'recall_macro'], cv=skf,
+                                n_jobs=-1)
 
-data_folds = pd.DataFrame(data=table)
+        accuracy += [(sum(scores["test_accuracy"]) / len(scores["test_accuracy"]))]
+        precision += [sum(scores["test_precision_macro"]) / len(scores["test_precision_macro"])]
+        recall += [(sum(scores["test_recall_macro"]) / len(scores["test_recall_macro"]))]
 
-print(data_folds.to_latex(caption="Sampler Performance",
-                          index=False,
-                          formatters={"name": str.upper},
-                          float_format="{:.4f}".format))
+    table = {'Name': names,
+             'Accuracy': accuracy,
+             'Precision': precision,
+             'Recall': recall}
+
+    data_folds = pd.DataFrame(data=table)
+
+    print(data_folds.to_latex(caption="Classifier Performance",
+                              index=False,
+                              formatters={"name": str.upper},
+                              float_format="{:.4f}".format))
+
+
+def vectorizer_performance():
+    """
+    Prints the statistics of each vectorizer in the list 'vectorizers' in LaTex format.
+
+    The function retrieves performance scores such as accuracy, precision, and recall from the dictionary 'scores'.
+    It creates a pandas DataFrame using the scores and then prints the DataFrame in LaTeX format.
+    """
+    vecotorizers = [
+        CountVectorizer(max_features=1000, min_df=0, max_df=0.9),
+        CountVectorizer(max_features=1000, min_df=0, max_df=0.9, stop_words=stopwords.words('english')),
+        TfidfVectorizer(max_features=1000, min_df=0, max_df=0.9),
+        TfidfVectorizer(max_features=1000, min_df=0, max_df=0.9, stop_words=stopwords.words('english')),
+    ]
+
+    names = ["Bag-of-words", "Bag-of-words (Remove Stopwords)", "TF-IDF", "TF-IDF (Remove Stopwords)"]
+    accuracy = []
+    precision = []
+    recall = []
+
+    for v, n in zip(vecotorizers, names):
+        steps = [
+            ('vect', v),
+            ('model', RandomForestClassifier()),
+        ]
+        pipeline = Pipeline(steps=steps)
+
+        # evaluate pipeline
+        scores = cross_validate(pipeline, X, y, scoring=['accuracy', 'precision_macro', 'recall_macro'], cv=skf,
+                                n_jobs=-1)
+
+        accuracy += [(sum(scores["test_accuracy"]) / len(scores["test_accuracy"]))]
+        precision += [sum(scores["test_precision_macro"]) / len(scores["test_precision_macro"])]
+        recall += [(sum(scores["test_recall_macro"]) / len(scores["test_recall_macro"]))]
+
+    table = {'Name': names,
+             'Accuracy': accuracy,
+             'Precision': precision,
+             'Recall': recall}
+
+    data_folds = pd.DataFrame(data=table)
+
+    print(data_folds.to_latex(caption="Vectorizer Performance",
+                              index=False,
+                              formatters={"name": str.upper},
+                              float_format="{:.4f}".format))
+
+
+def sampler_performance():
+    """
+    Prints the statistics of 'SMOTE oversampling' and 'Random Undersampling' in LaTex format.
+
+    The function retrieves performance scores such as accuracy, precision, and recall from the dictionary 'scores'.
+    It creates a pandas DataFrame using the scores and then prints the DataFrame in LaTeX format.
+    """
+    samplers = [
+        SMOTE(),
+        RandomUnderSampler()
+    ]
+
+    names = ["SMOTE oversampling", "Random Undersampling"]
+    accuracy = []
+    precision = []
+    recall = []
+
+    for s, n in zip(samplers, names):
+        steps = [
+            ('vect', TfidfVectorizer(max_features=1000, min_df=0, max_df=0.9)),
+            ('samper', s),
+            ('model', RandomForestClassifier()),
+        ]
+        pipeline = Pipeline(steps=steps)
+
+        # evaluate pipeline
+        scores = cross_validate(pipeline, X, y, scoring=['accuracy', 'precision_macro', 'recall_macro'], cv=skf,
+                                n_jobs=-1)
+
+        accuracy += [(sum(scores["test_accuracy"]) / len(scores["test_accuracy"]))]
+        precision += [sum(scores["test_precision_macro"]) / len(scores["test_precision_macro"])]
+        recall += [(sum(scores["test_recall_macro"]) / len(scores["test_recall_macro"]))]
+
+    table = {'Name': names,
+             'Accuracy': accuracy,
+             'Precision': precision,
+             'Recall': recall}
+
+    data_folds = pd.DataFrame(data=table)
+
+    print(data_folds.to_latex(caption="Sampler Performance",
+                              index=False,
+                              formatters={"name": str.upper},
+                              float_format="{:.4f}".format))
+
+
+fold_performance()
+classifier_performance()
+vectorizer_performance()
+sampler_performance()
